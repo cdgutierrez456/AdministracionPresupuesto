3dd5d19fb3c15bf826ea23f4dc5c15057348d0ab
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-07-01 16:09:33 -0500

    Creando clases principales. Agregando valores de presupuesto y restante inicial al html

--- js/app.js
+++ js/app.js
@@ -11,16 +11,41 @@ function eventListeners() {
 
 
 // Clases
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
 
+class UI {
+    insertarPresupuesto(cantidad) {
+        // Extrayendo valores
+        const {presupuesto, restante} = cantidad;
+
+        // Agregando al html
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
+}
+
+// Instanciando UI
+const ui = new UI();
+let presupuesto;
 
 // Funciones
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿Cual es tu presupuesto?');
-    console.log(presupuestoUsuario);
 
     if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
         window.location.reload();
     }
+
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto);
+
+    ui.insertarPresupuesto(presupuesto);
 }
 
 
